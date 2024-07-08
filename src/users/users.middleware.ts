df052d53988c99fdb597047e4930211b47d56ee3
Akshat <iamakku0.0.0.1@gmail.com>
2024-07-08 23:58:59 +0530

    some update in user middleware

--- src/users/users.middleware.ts
+++ src/users/users.middleware.ts
@@ -3,6 +3,13 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class UsersMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    const protocol = req.protocol;
+    const host = req.get("host")//local host
+    let url = req.originalUrl;
+    var method = req.method;
+    let date = new Date().toISOString();
+
+    console.log(protocol,host,url,method,date)
     console.log("users module specific middleware")
     next();
   }
