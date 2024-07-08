56c22671b9a7873160161041523c3ec3625ab200
Akshat <iamakku0.0.0.1@gmail.com>
2024-07-08 23:53:30 +0530

    implemented middlewares for user specific module only

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersMiddleware } from './users.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {
+export class UsersModule implements NestModule {
     constructor(){
         console.log('user module')
+    }
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(UsersMiddleware).forRoutes('users/addUser')
     }
 }
