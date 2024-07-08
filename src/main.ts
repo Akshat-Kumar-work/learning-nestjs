24ed4b070e77493bc6243f35ff7ed044ed0e6342
Akshat <iamakku0.0.0.1@gmail.com>
2024-07-08 23:40:54 +0530

    implemented global middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { RootModule } from './root.module';
+import { Response,Request,NextFunction } from 'express';
+
+function globalMiddleware1(req:Request,res:Response,next:NextFunction){
+  console.log("this is global middleware number one")
+  next();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
+  app.use(globalMiddleware1);
   await app.listen(3000);
 }
 bootstrap();
--- src/root.module.ts
+++ src/root.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
 import { ChatModule } from './chat/chat.module';
-import { BookController } from './book/book.controller';
-import { BookService } from './book/book.service';
 import { BookModule } from './book/book.module';
 
 @Module({
